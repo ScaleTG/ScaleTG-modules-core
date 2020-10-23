eb5966c53b6b5f87ed407beead208c9bc15b6fcb
Mike <mohammadhoseinmazalahi@gmail.com>
2020-10-23 20:48:21 +0330

    add minor documentation to config example

--- config.example.py
+++ config.example.py
@@ -9,7 +9,11 @@ USE https://github.com/ScaleTG-modules-core instead
 bot_token = '' # Obtain from t.me/BotFather
 bot_username = '' # (without @) Not required, but used by default authorization app
 
-# Currently enabled app, there must be a matching directory name in $CWD/apps/
+"""Currently enabled app, there must be a matching directory name in $CWD/apps/
+For every request, it is expected that only one of the following will return a response,
+otherwise, the first response returned is used. 
+Prioritize your apps accordingly.
+"""
 enabled_apps = [
     'authorization',
 ]
